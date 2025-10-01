527f8a25935f438e0a2f6cb50cb57f9cdeeea618
Gaareth <gareth04@outlook.de>
2025-10-01 13:05:54 +0200

    Update PathHistogram labels to reflect percentage

--- web-server/src/components/PathHistogram.tsx
+++ web-server/src/components/PathHistogram.tsx
@@ -63,7 +63,7 @@ const MyChart = ({
             labels,
             datasets: [
                 {
-                    label: "Pages reachable",
+                    label: "% Pages reachable",
                     data: data_points,
                 },
             ],
@@ -100,7 +100,7 @@ const MyChart = ({
                 </button>
             </div>
             <p class="text-base text-secondary">
-                Amount of pages reachable by clicking a link x times (length of
+                Percentage of pages reachable by clicking a link x times (length of
                 shortest paths)
             </p>
             <div>
