7d0019999db2b4cef712c6de5b4eb5565b1e555d
Gaareth <gareth04@outlook.de>
2025-10-08 13:58:19 +0200

    Bump version to 0.3.0

--- web-server/deploy.sh
+++ web-server/deploy.sh
@@ -13,19 +13,22 @@ source ../.env
 # upgrade patch?version
 # jq '.version |= (. | split(".") | .[2] = ((.[2] | tonumber) + 1 | tostring) | join("."))' package.json | sponge package.json
 
-git push   # push first
-
 echo "> Select version bump type:"
 select bump in patch minor major; do
     if [[ -n "$bump" ]]; then
-        npm version "$bump";
-        git add package.json package-lock.json; git commit -m "Bump version to $(jq -r .version package.json)";
-        git push --no-verify && git push --tags  # push commit and tag
+        echo "Selected bump type: $bump"/
         break
     else
         echo "Invalid selection."
     fi
 done
+
+git push
+
+# if push worked bump version
+npm version "$bump";
+git add package.json package-lock.json; git commit -m "Bump version to $(jq -r .version package.json)";
+git push --no-verify && git push --tags  # push commit and tag
 
 # rsync --exclude node_modules --exclude dist --delete -v -r ./* ${DEPLOY_HOST}:${DEPLOY_LOCATION}/web-server-test-environment;
 ssh ${DEPLOY_HOST} "cd ${DEPLOY_LOCATION}; ./trigger-update.sh"
