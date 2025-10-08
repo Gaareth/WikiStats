69cbed05525351ffc51fbf0cf129f20c5e9d6440
Gaareth <gareth04@outlook.de>
2025-10-08 16:12:13 +0200

    Test

--- web-server/astro.config.mjs
+++ web-server/astro.config.mjs
@@ -5,6 +5,15 @@ import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
+import { loadEnv } from "vite";
+
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), '');
+
+console.log("process", process.env.SITE);
+console.log("meta", import.meta.env.SITE);
+
+console.log("env", env.SITE);
+
 
 // https://astro.build/config
 export default defineConfig({
